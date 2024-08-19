c6c8131fb5b54bae3ef919fd60941e0234d17a26
kriomanser2000 <160469777+kriomanser2000@users.noreply.github.com>
2024-08-19 17:58:47 +0300

    Add files via upload

---
+++ sideMenu.php
@@ -0,0 +1,40 @@
+<?php
+class sideMenu
+{
+    public $name;
+    public $url;
+    public function __construct($name, $url)
+    {
+        $this->name = $name;
+        $this->url = $url;
+    }
+}
+class Menu
+{
+    private $listItems = [];
+    public function AddMenuItem($name, $url)
+    {
+        $sideMenu = new sideMenu($name, $url);
+        $this->listItems[] = $sideMenu;
+    }
+    public function PrintMenu()
+    {
+        echo "<ul style='list-style: none; padding: 0; margin: 0;'>";
+        foreach ($this->listItems as $item)
+        {
+            echo "<li style='background-color: #33acff; width: 200px; height: 70px; float: left; margin-right: 10px;'>";
+            echo "<a href='{$item->url}' style='display: block; height: 100%; text-decoration: none;'>";
+            echo "<p style='color: blue; margin: 0; padding-left: 30%; font-size: 30px; padding-top: 20px;'>{$item->name}</p>";
+            echo "</a>";
+            echo "</li>";
+        }
+        echo "</ul>";
+    }
+}
+$menu = new Menu();
+$menu->AddMenuItem("Home", "1.php");
+$menu->AddMenuItem("About", "2.php");
+$menu->AddMenuItem("Photo", "3.php");
+$menu->AddMenuItem("Contact Us", "4.php");
+$menu->AddMenuItem("Login", "5.php");
+$menu->PrintMenu(300, 400, "#f4f4f4", "#333333");
