5c870f3fdea5c1ada02c50d56f59567f3764246b
kriomanser2000 <160469777+kriomanser2000@users.noreply.github.com>
2024-08-21 12:10:46 +0300

    Add files via upload

---
+++ menuContact.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Us</title
+</head>
+<body>
+<h1>Mail - <a href="">blablabla15@gmail.com</a></h1>
+<h1>+380546754784</h1>
+</body>
+</html>
